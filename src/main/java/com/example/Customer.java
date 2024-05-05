564f6517268718ae848d219d3c7843bf284077e8
cometstellar <jabbarirhamabdul0@gmail.com>
2024-05-05 20:26:46 +0700

    update 0505 new

--- src/main/java/com/example/Customer.java
+++ src/main/java/com/example/Customer.java
@@ -2,176 +2,126 @@ package com.example;
 import java.time.LocalDate;
 import java.util.Scanner;
 
-public abstract class customer{
-
-    protected String firstName = null;
-    protected String lastName = null;
-    
 
 
-    protected customer(String firstName, String lastName){
+public abstract class Customer {
+    protected String firstName;
+    protected String lastName;
+    protected boolean hasOrder; 
+
+    public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.hasOrder = false; 
     }
 
     public String getFirstName() {
         return firstName;
-    }public String getLastName() {
+    }
+
+    public String getLastName() {
         return lastName;
     }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    
     }
 
-    public abstract void getFullName();
-    public static abstract void makeOrder();
-    public abstract void confirmPay();
-   
+    public abstract String getFullName();
+
+    public abstract void makeOrder();
+
+    public abstract void confirmPay(int orderNumber);
 }
 
-class Guest extends customer{
-
-    public Guest(String firstName, String lastName) {
+class Guest extends Customer {
+    public guest(String firstName, String lastName) {
         super(firstName, lastName);
     }
 
-
-
-   @Override
-   public void getFullName(){
-    if(lastName != null){
-        System.out.println("Customer Name: " + getFirstName() + " " + getLastName());
+    @Override
+    public String getFullName() {
+        if (lastName != null) {
+            return "Customer Name: " + getFirstName() + " " + getLastName();
+        } else {
+            return "Customer Name: " + getFirstName();
+        }
     }
-    else{
-        System.out.println("Customer Name: " + getFirstName());
+
+    @Override
+    public void makeOrder() {
+        if (!hasOrder) {
+            System.out.println("Guest makes an order");
+            hasOrder = true; 
+        } else {
+            System.out.println("Guest already has an active order.");
+        }
     }
-   }
 
-   @Override
-   public static void makeOrder() {
-       Order.getOrderNumber();
-       
-   }
-
+    @Override
+    public void confirmPay(int orderNumber) {
+        if (hasOrder) {
+            System.out.println("Guest confirms payment for order " + orderNumber);
+            hasOrder = false; 
+        } else {
+            System.out.println("Guest has no active order to confirm payment.");
+        }
+    }
 }
 
- class Member extends customer{
+class Member extends Customer {
     private String memberId;
     private LocalDate joinedDate;
 
-    private Member(String firstName, String lastName, String memberId, LocalDate joinedDate){
+    public Member(String firstName, String lastName, String memberId, LocalDate joinedDate) {
         super(firstName, lastName);
         this.memberId = memberId;
         this.joinedDate = joinedDate;
     }
 
-    private LocalDate getJoinedDate() {
+    public String getMemberId() {
+        return memberId;
+    }
+
+    public LocalDate getJoinedDate() {
         return joinedDate;
     }
 
-    public static Guest guest(Scanner in){
-        
+    @Override
+    public String getFullName() {
+        if (getLastName() != null) {
+            return "Customer Name: " + getFirstName() + " " + getLastName();
+        } else {
+            return "Customer Name: " + getFirstName();
+        }
     }
-    
 
     @Override
-    public void getFullName(){
-        if (getLastName() != null){
-            System.out.println("Customer Name: " + getFirstName() + " " + getLastName());
-        }
-        else{
-            System.out.println("Customer Name: " + getFirstName());
+    public void makeOrder() {
+        if (!hasOrder) {
+            System.out.println("Member makes an order");
+            hasOrder = true; 
+        } else {
+            System.out.println("Member already has an active order.");
         }
     }
 
-    
+    @Override
+    public void confirmPay(int orderNumber) {
+        if (hasOrder) {
+            System.out.println("Member confirms payment for order " + orderNumber);
+            hasOrder = false; 
+        } else {
+            System.out.println("Member has no active order to confirm payment.");
+        }
+    }
+
+    public int getMembershipDuration() {
+        long diff = LocalDate.now().toEpochDay() - joinedDate.toEpochDay();
+        return (int) (diff / 365);
+    }
 }
-
-    
-    
-
- 
-// class Customer {
-//     private String firstName;
-//     private String lastName;
-//     private String id;
-//     private String phoneNumber;
-//     private String email;
-
-//     public Customer(String firstName, String lastName, String id, String phoneNumber, String email) {
-//         this.firstName = firstName;
-//         this.lastName = lastName;
-//         this.id = id;
-//         this.phoneNumber = phoneNumber;
-//         this.email = email;
-//     }
-
-//     public String getFirstName() {
-//         return firstName;
-//     }
-
-//     public String getLastName() {
-//         return lastName;
-//     }    
-
-//     public String getId() {
-//         return id;
-//     }
-
-//     public String getPhoneNumber() {
-//         return phoneNumber;
-//     }
-
-//     public String getEmail() {
-//         return email;
-//     }
-// }
-
-// class Guest extends Customer {
-//     public Guest(String firstName, String lastName, String id, String phoneNumber, String email) {
-//         super(firstName, lastName, id, phoneNumber, email);
-//     }
-
-//     public static Guest guest(Scanner in) {
-//         System.out.print("Masukkan nama depan anda: ");
-//         String firstName = in.nextLine();
-//         System.out.println("Masukkan nama belakang anda: ");
-//         String lastName = in.next();
-//         System.out.print("Masukkan ID: ");
-//         String id = in.nextLine();
-//         System.out.print("Masukkan Nomor Telepon: ");
-//         String phoneNumber = in.nextLine();
-//         System.out.print("Masukkan Email: ");
-//         String email = in.nextLine();
-//         return new Guest(firstName, lastName, id, phoneNumber, email);
-//     }
-// }
-
-// class Member extends Customer {
-//     private static final String memberUsername = "apa_iyh";
-//     private static final String memberPassword = "tch";
-
-//     public Member(String firstName, String lastName, String id, String phoneNumber, String email) {
-//         super(firstName, lastName, id, phoneNumber, email);
-//     }
-
-//     public static Member memberLogin(Scanner in) {
-//         System.out.print("Masukkan Username: ");
-//         String username = in.nextLine();
-//         System.out.print("Masukkan Password: ");
-//         String password = in.nextLine();
-
-//         if (username.equals(memberUsername) && password.equals(memberPassword)) {
-//             System.out.println("Login berhasil!");
-//             return new Member("apa_iyh", "tch", "0812", "dev@");
-//         } else {
-//             System.out.println("Username atau password salah!");
-//             return null;
-//         }
-//     }
-// }
