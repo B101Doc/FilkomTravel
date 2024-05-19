465b825f6c6e43888e511e498b02ec5cf749a6ce
cometstellar <jabbarirhamabdul0@gmail.com>
2024-05-19 20:09:17 +0700

    Update 19/05 20.09

--- src/main/java/com/example/Promotion.java
+++ src/main/java/com/example/Promotion.java
@@ -10,14 +10,25 @@ abstract class Promotion implements Applicable, Comparable<Promotion> {
     protected Date endDate;
     protected double minimumOrderTotal;
     protected double discountPercent;
+    protected double promoShipCost;
 
 
-    public Promotion(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent) {
+    public Promotion(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent, double promoShipCost) {
         this.promoCode = promoCode;
         this.startDate = startDate;
         this.endDate = endDate;
         this.minimumOrderTotal = minimumOrderTotal;
         this.discountPercent = discountPercent;
+        this.promoShipCost = promoShipCost;
+    }
+
+
+    public double getPromoShipCost() {
+        return promoShipCost;
+    }
+
+    public void setPromoShipCost(double promoShipCost) {
+        this.promoShipCost = promoShipCost;
     }
 
     @Override
@@ -28,8 +39,7 @@ abstract class Promotion implements Applicable, Comparable<Promotion> {
     @Override
     public boolean isCustomerEligible(Customer customer) {
         if (customer instanceof Member) {
-            Member member = (Member) customer;
-            int membershipDuration = member.getMembershipDuration();
+            int membershipDuration = Member.getMembershipDuration();
             return membershipDuration > 30;
         }
         return false;
@@ -59,8 +69,8 @@ class PercentOffPromo extends Promotion
 {
 
 
-    public PercentOffPromo(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent) {
-        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent);
+    public PercentOffPromo(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent, double promoShipCost) {
+        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent, promoShipCost);
     }
 
     @Override
@@ -86,8 +96,8 @@ class PercentOffPromo extends Promotion
 
 class Cashback extends Promotion
 {
-    public Cashback(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent) {
-        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent);
+    public Cashback(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent, double promoShipCost) {
+        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent, promoShipCost);
     }
 
     @Override
@@ -116,8 +126,8 @@ class DeliveryFeePromo extends Promotion
 {
     public static List<Promotion> promotions = new ArrayList<Promotion>();
 
-    public DeliveryFeePromo(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent) {
-        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent);
+    public DeliveryFeePromo(String promoCode, Date startDate, Date endDate, double minimumOrderTotal, double discountPercent, double promoShipCost) {
+        super(promoCode, startDate, endDate, minimumOrderTotal, discountPercent, promoShipCost);
     }
 
 
@@ -137,7 +147,12 @@ class DeliveryFeePromo extends Promotion
     }
 
     @Override
-    public double calculateShippingFeeDiscount(Order order) {
+    public double calculateShippingFeeDiscount(Order order, Member member, Customer customer) {
+        if(order.getShipCost() == 10000 && customer instanceof Member){
+            promoShipCost = 5000;
+        } else if (order.getShipCost() == 20000 && customer instanceof Member) {
+            promoShipCost = 7500;
+        }
         return 0;
     }
 }
